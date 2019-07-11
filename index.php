111de7e35c44c7183eceafbea461f6a6d7f515a9
mustmoveon <52797525+mustmoveon@users.noreply.github.com>
2019-07-11 19:58:02 +0200

    index.php
    Check index file

---
+++ index.php
@@ -0,0 +1,6 @@
+echo "# mustmoveon" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/mustmoveon/mustmoveon.git
+git push -u origin master
